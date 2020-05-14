f81baec8aa7b2df5a089285fdf1b06fd5348f53b
Craig Carlson <craigcarlson@Craigs-Mac-Pro.local>
2020-05-13 23:53:53 -0500

    working interactable buttons no functions timer counts down nothing happens

---
+++ README.md
@@ -0,0 +1,16 @@
+**Info**
+
+This project is a pomodoro clock built using vanilla JavaScript.
+
+The Pomodoro Technique is a time management system where you work for a period of time then take a short break, then work for a period of time then take a short break, and so on. You learned about it in the Gearing Up lesson. This is also a useful system for switching between Driver and Navigator roles when pairing.
+
+We are going to make a Pomodoro clock similar to this which will count down twenty-five minutes of work, indicate it is break time, count down five minutes, then start over again. If you've been following along the curriculum, you should have most of the tools you will need to make it. But if you do have problems, there are two of you! Put your minds together and figure it out.
+
+___________
+
+**User Stories**
+
+1: I can start a new work/rest cycle;
+2: I can stop a new work/rest cycle;
+3: I can pause a new cycle;
+4: I can adjust the length of the cycle;
---
+++ main.js
@@ -0,0 +1,46 @@
+console.log("hello world!");
+
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const pauseButton = document.querySelector('[data-pause]');
+
+startButton.addEventListener('click', () => {
+    // console.log("Yeah got fuckin clicked right here.");
+    console.log("startButton clicked");
+})
+
+stopButton.addEventListener('click', () => {
+    console.log("stop buttpns clicked");
+})
+
+pauseButton.addEventListener('click', () => {
+    console.log("pauseButton clicked");
+})
+
+
+var countDownDate = new Date().getTime() + (60000 * 25)
+// Update the count down every 1 second
+var x = setInterval(function () {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
