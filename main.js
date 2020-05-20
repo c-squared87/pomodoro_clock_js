021163da9d228fc4fd4946c35ccd4c1f2aef730f
Craig Carlson <craigcarlson@Craigs-Mac-Pro.local>
2020-05-20 14:03:28 -0500

    start timer kinda of works and updates appriate fields

--- main.js
+++ main.js
@@ -1,80 +1,75 @@
 // JUST MAKING SURE THIS ALL WORKs.
 // console.log("hello world!");
 
-class PomoClock {
 
-    constructor(mainMin, mainSec, workDisp, restDisp) {
-        this.mainMinutes = mainMin;
-        this.mainSeconds = mainSec;
-        this.workDisplay = workDisp;
-        this.restDisplay = restDisp;
+function startTimer() {
 
-        // this.mainMinutes.innerText = "22:22";
-    }
+    console.log("Starting a new session");
 
-    startTimer() {
-        var countDownDate = new Date().getTime() + (60000 * 25)
+    var countDownDate = new Date().getTime() + (60000 * 25)
 
-        // Update the count down every 1 second
-        var x = setInterval(function () {
+    // Update the count down every 1 second
+    var x = setInterval(function () {
 
-            // Get today's date and time
-            var now = new Date().getTime();
+        // Get today's date and time
+        var now = new Date().getTime();
 
-            // Find the distance between now and the count down date
-            var distance = countDownDate - now;
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
 
-            // // Time calculations for days, hours, minutes and seconds
-            // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-            // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        // // Time calculations for days, hours, minutes and seconds
+        // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-            // Display the result in the element with id="demo" TODO: CHANGE FROM 'DEMO'
-            // document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-            //     + minutes + "m " + seconds + "s ";
-            this.mainMinutes = minutes + " : ";
-            this.mainSeconds = seconds;
-            // If the count down is finished, write some text
-            // if (distance < 0) {
-            //     clearInterval(x);
-            //     document.getElementById("demo").innerHTML = "EXPIRED";
-            // }
-        }, 1000);
-    }
+        mainTimerMinutes.innerText = minutes.toString();
+        mainTimerSeconds.innerText = seconds.toString();
 
-    pauseTimer() {
+        // Display the result in the element with id="demo" TODO: CHANGE FROM 'DEMO'
+        // document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+        //     + minutes + "m " + seconds + "s ";
 
-    }
-
-    resetTimer() {
-
-    }
-
-    modifyWorkTime(interval) {
-        // this.workTime += interval;
-        this.updateWorkTimeDisplay();
-    }
-
-    modifyRestTime(interval) {
-        // this.restTime += interval;
-        this.updateRestTimeDisplay();
-    }
-
-    updateWorkTimeDisplay() {
-        this.workDisplay.innerText =
-    }
-
-    updateRestTimeDisplay() {
-        this.restDisplay.innerText = "testing resting";
-    }
-
-    updateMainTimer() {
-
-    }
-
-
+        // If the count down is finished, write some text
+        // if (distance < 0) {
+        //     clearInterval(x);
+        //     document.getElementById("demo").innerHTML = "EXPIRED";
+        // }
+    }, 1000);
 }
+
+// pauseTimer() {
+
+// }
+
+// resetTimer() {
+
+// }
+
+// modifyWorkTime(interval) {
+//     // this.workTime += interval;
+//     this.updateWorkTimeDisplay();
+// }
+
+// modifyRestTime(interval) {
+//     // this.restTime += interval;
+//     this.updateRestTimeDisplay();
+// }
+
+// updateWorkTimeDisplay() {
+//     this.workDisplay.innerText = "testing this shits right here";
+// }
+
+// updateRestTimeDisplay() {
+//     this.restDisplay.innerText = "testing resting";
+// }
+
+// updateMainTimer() {
+
+// }
+
+
+
 
 // Fields
 const mainTimerMinutes = document.querySelector('[data-main-mins]');
@@ -94,13 +89,13 @@ const increaseRestTimeBtn = document.querySelector('[data-increase-rest-button]'
 const decreaseRestTimeBtn = document.querySelector('[data-decrease-rest-button]');
 
 // Make a new clock to play with.
-const clock = new PomoClock(mainTimerMinutes, mainTimerSeconds, workLengthDisplay, restLengthDisplay);
+// const clock = new PomoClock(mainTimerMinutes, mainTimerSeconds, workLengthDisplay, restLengthDisplay);
 
 // Listeners
 
 // TODO: ALL THIS SHITE NEEDS TO BE working.
 startButton.addEventListener('click', () => {
-    clock.startTimer();
+    startTimer();
 })
 pauseButton.addEventListener('click', () => {
     clock.pauseTimer;
@@ -117,34 +112,3 @@ increaseRestTimeBtn.addEventListener('click', () => {
 decreaseRestTimeBtn.addEventListener('click', () => {
     clock.modifyRestTime(-1);
 })
-
-
-// TODO: REWORK ALL OF THIS BELOW.
-
-// var countDownDate = new Date().getTime() + (60000 * 25)
-
-// // Update the count down every 1 second
-// var x = setInterval(function () {
-
-//     // Get today's date and time
-//     var now = new Date().getTime();
-
-//     // Find the distance between now and the count down date
-//     var distance = countDownDate - now;
-
-//     // Time calculations for days, hours, minutes and seconds
-//     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-//     // Display the result in the element with id="demo" TODO: CHANGE FROM 'DEMO'
-//     document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-//         + minutes + "m " + seconds + "s ";
-
-//     // If the count down is finished, write some text
-//     if (distance < 0) {
-//         clearInterval(x);
-//         document.getElementById("demo").innerHTML = "EXPIRED";
-//     }
-// }, 1000);
